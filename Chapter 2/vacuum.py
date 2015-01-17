5a0a96dc441a58f4890a095c9da3a90c903b2ef5
slray <steve.l.ray@gmail.com>
2015-01-17 12:50:24 -0500

    Stubbed in vacuum API

---
+++ Chapter 2/vacuum.py
@@ -0,0 +1,19 @@
+class vacuum:
+    #static variables
+    
+    #constructror
+    def __init__(self):
+        #class variables
+        pass #NoOp
+    
+    def left(self):
+        pass
+    
+    def right(self):
+        pass
+    
+    def suck(self, ):
+        pass
+    
+    def NoOp(self):
+        pass
