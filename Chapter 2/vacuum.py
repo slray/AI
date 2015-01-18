dfc99d30d7a33c1c41c1931d3b63548737812ec6
Jularie1949 <lawrenceray@Lawrence-Rays-Mac-mini-4.local>
2015-01-18 10:40:06 -0500

    Track moves and sucks

--- Chapter 2/vacuum.py
+++ Chapter 2/vacuum.py
@@ -1,18 +1,33 @@
+from Environment import Environment
+
 class vacuum:
     #static variables
-    
-    #constructror
-    def __init__(self):
+        
+    #constructor
+    def __init__(self, Environment = None):
         #class variables
-        pass #NoOp
-    
+        self.environment = Environment
+        if not self.environment:
+            self.environment = Environment()
+        self.Moves = 0
+        self.SuckUps = 0
+        self.GoodSucks = 0
+     
     def left(self):
+        self.Moves += 1
+        self.environment.MoveLeft()
         pass
     
     def right(self):
+        self.Moves += 1
+        self.environment.MoveRight()
         pass
     
     def suck(self):
+        if (self.environment.CheckRoom()):
+            self.GoodSucks += 1
+        self.SuckUps += 1
+        self.environment.Action("S")
         pass
     
     def NoOp(self):
